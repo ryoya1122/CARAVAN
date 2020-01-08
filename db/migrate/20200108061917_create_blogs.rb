d78560164eed8f0e32641d5dde0d57cc13cc78e1
ryoya1122 <koba.jht@gmail.com>
2020-01-08 15:42:45 +0900

    [Finish] 4-2

--- db/migrate/20200108061917_create_blogs.rb
+++ db/migrate/20200108061917_create_blogs.rb
@@ -3,7 +3,7 @@ class CreateBlogs < ActiveRecord::Migration[5.2]
     create_table :blogs do |t|
       t.string :title
       t.string :category
-      t.text　 :body
+      t.text :body
 
       t.timestamps
     end
